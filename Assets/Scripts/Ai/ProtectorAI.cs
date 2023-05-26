7ce7007a022518a02311eb337019baaf57ea2255
3tonopa <91850576+3tonopa@users.noreply.github.com>
2023-05-26 20:36:53 +0300

    lesson6

---
+++ Assets/Scripts/Ai/ProtectorAI.cs
@@ -0,0 +1,53 @@
+﻿using Pathfinding;
+using UnityEngine;
+namespace Platformer
+{
+public class ProtectorAI : IProtector
+{
+    private readonly LevelObjectView _view;
+    private readonly PatrolAIModel _model;
+    private readonly AIDestinationSetter _destinationSetter;
+    private readonly AIPatrolPath _patrolPath;
+  
+    private bool _isPatrolling;
+ 
+    public ProtectorAI(LevelObjectView view, PatrolAIModel model, AIDestinationSetter destinationSetter, AIPatrolPath patrolPath)
+    {
+        _view = view;
+        _model = model;
+        _destinationSetter = destinationSetter;
+        _patrolPath = patrolPath;
+    }
+
+    public void Init()
+    {
+        _destinationSetter.target = _model.GetNextTarget();
+        _isPatrolling = true;
+        _patrolPath.TargetReached += OnTargetReached;
+    }
+
+    public void Deinit()
+    {
+        _patrolPath.TargetReached -= OnTargetReached;
+    }
+    
+    private void OnTargetReached()
+    {
+        _destinationSetter.target = _isPatrolling
+            ? _model.GetNextTarget()
+            : _model.GetClosestTarget(_view.transform.position);
+    }
+  
+    public void StartProtection(GameObject invader)
+    {
+        _isPatrolling = false;
+        _destinationSetter.target = invader.transform;
+    }
+
+    public void FinishProtection(GameObject invader)
+    {
+        _isPatrolling = true;
+        _destinationSetter.target = _model.GetClosestTarget(_view.transform.position);
+    }
+}
+}
